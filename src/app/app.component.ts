5fc140bf9e16d400e7002b54f58bd104810acf86
Roberto Vannucci <robertovannucci.dev@gmail.com>
2020-03-24 10:13:28 +0100

    nuova versione autodichiarazione 23 marzo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,12 @@ export class AppComponent implements OnInit {
 
   reasons = [
     'Comprovate esigenze lavorative',
-    'Situazioni di necessità',
+    `Assoluta urgenza (“per trasferimenti in comune diverso”, come previsto
+     dall’art. 1, comma 1, lettera b) del Decreto del Presidente del Consiglio dei
+     Ministri 22 marzo 2020)`,
+    `Situazione di necessita' (per spostamenti all’interno dello stesso comune,
+     come previsto dall’art. 1, comma 1, lett. a) del Decreto del Presidente del
+     Consiglio dei Ministri 8 marzo 2020`,
     'Motivi di salute',
     'Rientro presso il proprio domicilio, abitazione o residenza'
   ];
@@ -40,6 +45,8 @@ export class AppComponent implements OnInit {
     docNumber : '',
     reason : '',
     statement : '',
+    addressStart : '',
+    addressEnd : '',
 
     birthDate : { date: null, formatted : ''  }
   };
@@ -57,12 +64,17 @@ export class AppComponent implements OnInit {
   }
 
   validate(): boolean {
-    for(let f of Object.keys(this.userData)){
+    for (const f of Object.keys(this.userData)) {
       if (!this.userData[f]) {
-        console.log("invalid: ", f);
+        console.log('invalid: ', f);
         alert('Attenzione! Tutti i campi sono obbligatori');
         return false;
       }
+    }
+
+    if (this.reasons.indexOf(this.userData.reason) < 0) {
+      alert('Attenzione! Motivazione dello spostamento obbligatoria');
+      return false;
     }
 
     return true;
@@ -94,32 +106,32 @@ export class AppComponent implements OnInit {
 
 
     const secondTexts = [
-      `> di essere a conoscenza delle misure di contenimento del contagio
-      di cui al combinato disposto dell'art. 1 del Decreto del Presidente del Consiglio dei
-      Ministri 8 marzo 2020 e dell'art. 1, comma 1, del Decreto del Presidente del Consiglio
-      dei Ministri del 9 marzo 2020 concernenti lo spostamento delle persone fisiche
-      all'interno di tutto il territorio nazionale;`,
-      `> di non essere sottoposto alla misura della quarantena e di non essere
-      risultato positivo al virus COVID-19 di cui all'articolo 1, comma 1, lettera c),
-      del Decreto del Presidente del Consiglio dei Ministri dell' 8 marzo 2020;`,
-      `> di essere a conoscenza delle sanzioni previste, dal combinato disposto
-      dell'art. 3, comma 4, del D.L. 23 febbraio 2020, n. 6 e dell'art. 4, comma
-      2, del Decreto del Presidente del Consiglio dei Ministri dell' 8 marzo 2020
-      in caso di inottemperanza delle predette misure di contenimento
-      (art. 650 c.p. salvo che il fatto non costituisca piu\` grave reato);`
+      `> di essere a conoscenza delle misure di contenimento del contagio previste dall’art. 1 del Decreto del Presidente del Consiglio
+      dei Ministri 11 marzo 2020, l’art. 1 del Decreto del Presidente del Consiglio dei Ministri 22 marzo 2020, dall’art. 1 dell’Ordinanza
+      del Ministro della salute 20 marzo 2020 concernenti le limitazioni alle possibilita' di spostamento delle persone fisiche all’interno
+      di tutto il territorio nazionale;`,
+      `> di non essere sottoposto alla misura della quarantena e di non essere risultato positivo al COVID-19 di cui all’articolo 1,
+      comma 1, lettera c), del Decreto del Presidente del Consiglio dei Ministri 8 marzo 2020;`,
+      `> di essere a conoscenza delle sanzioni previste dal combinato disposto dell’art. 3, comma 4, del decreto legge 23
+      febbraio 2020, n. 6 e dell’art. 4, comma 2, del Decreto del Presidente del Consiglio dei Ministri 8 marzo 2020
+      in caso di inottemperanza delle predette misure di contenimento (art. 650 c.p. salvo che il fatto non costituisca piu' grave reato);`,
+      `> che lo spostamento e' iniziato da ${this.userData.addressStart} con destinazione ${this.userData.addressEnd}`
     ];
 
-    const secondText = secondTexts.map(x => x.replace(/\n/g, '').replace(/ {2}/g, ' ')).join('\n')
+    doc.setFontSize(14);
+
+    const secondText = secondTexts.map(x => x.replace(/\n/g, '').replace(/ {2}/g, ' ')).join('\n');
 
     doc.text(10, 15, doc.splitTextToSize(firstText, ROW_WIDTH));
 
-    doc.text(38, 60, 'DICHIARA SOTTO LA PROPRIA RESPONSABILITÀ');
+    doc.text(38, 50, 'DICHIARA SOTTO LA PROPRIA RESPONSABILITÀ');
 
-    doc.text(10, 80, doc.splitTextToSize(secondText, ROW_WIDTH));
+    doc.text(10, 60, doc.splitTextToSize(secondText, ROW_WIDTH));
 
 
-    doc.text(10, 170, '> Che lo spostamento è determinato da: ');
-    let y = 180;
+    doc.text(10, 155, '> Che lo spostamento è determinato da: ');
+
+    let y = 165;
 
     this.reasons.forEach(r => {
       if (r === this.userData.reason) {
@@ -128,12 +140,12 @@ export class AppComponent implements OnInit {
         doc.text(25, y, `( )  ${r}` );
       }
 
-      y += 8;
+      y += 7 * r.split('\n').length;
     });
 
-    y += 10;
+    y += 8;
 
-    const statementText = doc.splitTextToSize(`A questo proposito dichiara che:\n${this.userData.statement}`, 180);
+    const statementText = doc.splitTextToSize(`A questo proposito dichiara che:\n${this.userData.statement}`, ROW_WIDTH);
 
     doc.text(15, y, statementText);
 
